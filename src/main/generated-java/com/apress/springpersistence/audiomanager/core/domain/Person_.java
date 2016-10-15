2e6acfab094f69e4ee0f7befb5df7231a9ce19c9
Benjamin Slabbert <benjamin@impactradius.com>
2016-10-15 17:03:53 +0200

    adding generated classes

---
+++ src/main/generated-java/com/apress/springpersistence/audiomanager/core/domain/Person_.java
@@ -0,0 +1,14 @@
+package com.apress.springpersistence.audiomanager.core.domain;
+
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@StaticMetamodel(Person.class)
+public abstract class Person_ extends com.apress.springpersistence.audiomanager.core.domain.Thing_ {
+
+	public static volatile SingularAttribute<Person, String> gender;
+	public static volatile SingularAttribute<Person, String> telephone;
+	public static volatile SingularAttribute<Person, String> email;
+
+}
+
---
+++ src/main/generated-java/com/apress/springpersistence/audiomanager/core/domain/Thing_.java
@@ -0,0 +1,21 @@
+package com.apress.springpersistence.audiomanager.core.domain;
+
+import java.net.URL;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@StaticMetamodel(Thing.class)
+public abstract class Thing_ {
+
+	public static volatile SingularAttribute<Thing, URL> image;
+	public static volatile SingularAttribute<Thing, URL> additionalType;
+	public static volatile SingularAttribute<Thing, String> name;
+	public static volatile SingularAttribute<Thing, String> description;
+	public static volatile SingularAttribute<Thing, String> alternateName;
+	public static volatile SingularAttribute<Thing, Long> id;
+	public static volatile SingularAttribute<Thing, Integer> version;
+	public static volatile SingularAttribute<Thing, URL> url;
+	public static volatile SingularAttribute<Thing, URL> sameAs;
+
+}
+
